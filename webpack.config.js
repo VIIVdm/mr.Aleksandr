58283f73f97604c57d95f13c640c491eb5d655df
Viktoriya Baturina <vii.sb@mail.ru>
2023-11-09 15:27:15 +0300

    add block navBar and aboutMe

--- webpack.config.js
+++ webpack.config.js
@@ -49,6 +49,8 @@ module.exports = (env, argv) => {
             patterns: [
               { from: path.resolve(__dirname, 'src', 'favicon.ico'), 
               to: path.resolve(__dirname, 'dist')},
+              { from: path.resolve(__dirname, 'src/img'), 
+              to: path.resolve(__dirname, 'dist/img')}
             ],
         }),
         new MiniCssExtractPlugin({
